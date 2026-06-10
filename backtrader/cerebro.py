9e5eca868a190c91eac7de8fa159857e452ddd30
kikomiguelmendes <kikomiguelmendes@gmail.com>
2026-06-10 11:18:40 +0200

    changes 1 created

--- backtrader/cerebro.py
+++ backtrader/cerebro.py
@@ -1506,8 +1506,6 @@ class Cerebro(with_metaclass(MetaParams, object)):
         data0 = datas[0]
         d0ret = True
 
-        rs = [i for i, x in enumerate(datas) if x.resampling]
-        rp = [i for i, x in enumerate(datas) if x.replaying]
         rsonly = [i for i, x in enumerate(datas)
                   if x.resampling and not x.replaying]
         onlyresample = len(datas) == len(rsonly)
@@ -1516,11 +1514,13 @@ class Cerebro(with_metaclass(MetaParams, object)):
         clonecount = sum(d._clone for d in datas)
         ldatas = len(datas)
         ldatas_noclones = ldatas - clonecount
-        lastqcheck = False
         dt0 = date2num(datetime.datetime.max) - 2  # default at max
         while d0ret or d0ret is None:
             # if any has live data in the buffer, no data will wait anything
-            newqcheck = not any(d.haslivedata() for d in datas)
+            if self.params.live: 
+                newqcheck = not any(d.haslivedata() for d in datas)
+            else: 
+                newqcheck = False
             if not newqcheck:
                 # If no data has reached the live status or all, wait for
                 # the next incoming data
@@ -1541,14 +1541,18 @@ class Cerebro(with_metaclass(MetaParams, object)):
             # from the qcheck value
             drets = []
             qstart = datetime.datetime.utcnow()
+            qlapse = datetime.datetime.utcnow() - qstart
             for d in datas:
-                qlapse = datetime.datetime.utcnow() - qstart
                 d.do_qcheck(newqcheck, qlapse.total_seconds())
                 drets.append(d.next(ticks=False))
 
-            d0ret = any((dret for dret in drets))
-            if not d0ret and any((dret is None for dret in drets)):
-                d0ret = None
+            d0ret = False
+            for dret in drets:
+                if dret:
+                    d0ret = True
+                    break
+                elif dret is None:
+                    d0ret = None
 
             if d0ret:
                 dts = []
@@ -1557,12 +1561,12 @@ class Cerebro(with_metaclass(MetaParams, object)):
 
                 # Get index to minimum datetime
                 if onlyresample or noresample:
-                    dt0 = min((d for d in dts if d is not None))
+                    dt0, dt0_idx = min(((d, i) for i, d in enumerate(dts) if d is not None), key=lambda x: x[0])
                 else:
-                    dt0 = min((d for i, d in enumerate(dts)
-                               if d is not None and i not in rsonly))
+                    dt0, dt0_idx = min(((d, i) for i, d in enumerate(dts)
+                        if d is not None and i not in rsonly), key=lambda x: x[0])
 
-                dmaster = datas[dts.index(dt0)]  # and timemaster
+                dmaster = datas[dt0_idx]  # and timemaster
                 self._dtmaster = dmaster.num2date(dt0)
                 self._udtmaster = num2date(dt0)
 
@@ -1586,10 +1590,8 @@ class Cerebro(with_metaclass(MetaParams, object)):
                 for i, dti in enumerate(dts):
                     if dti is not None:
                         di = datas[i]
-                        rpi = False and di.replaying   # to check behavior
                         if dti > dt0:
-                            if not rpi:  # must see all ticks ...
-                                di.rewind()  # cannot deliver yet
+                            di.rewind()  # cannot deliver yet
                             # self._plotfillers[i].append(slen)
                         elif not di.replaying:
                             # Replay forces tick fill, else force here
